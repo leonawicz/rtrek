c0a75e957d88568adee80db50015904e309252d8
leonawicz <matt.rstat@gmail.com>
2018-05-16 18:13:21 -0600

    Skip test requiring browser on CRAN and Travis.

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -12,5 +12,8 @@ d <- st_book_series()
 test_that("st_book_series returns as expected", {
   expect_is(d, "data.frame")
   expect_equal(dim(d), c(17, 2))
+
+  skip_on_cran()
+  skip_on_travis()
   expect_is(st_book_series("DS9"), "NULL")
 })
