0529be0fb5b0c8269ea0f8cf19174af18d37ff27
leonawicz <matt.rstat@gmail.com>
2018-05-17 18:25:00 -0600

    Update hex logo script.

--- data-raw/hex.R
+++ data-raw/hex.R
@@ -1,17 +1,22 @@
 #install.packages("hexSticker")
 library(hexSticker)
-library(ggplot2)
+library(memery)
 pkg <- basename(getwd())
-#img <- "data-raw/hexsubplot.png" # add custom subplot here
-out <- paste0(c("data-raw/", "inst/"), pkg, ".png")
+out <- paste0(c("data-raw/images/"), pkg, c(".png", "-small.png"))
+sysfonts::font_add(family = "tng", "data-raw/Star Next.ttf")
+meme("data-raw/images/ufp0.png", "R", "data-raw/images/hexsubplot.png", size = 13,
+     label_pos = list(w = 1, h = 1, x = 0.43, y = 0.5), family = "tng", col = "#98F5FFDD", shadow = "transparent")
+
+round(663*2400/2074) # Next stretch width to 767 pixels and resave as hexsubplot2.png, then continue with script.
 
 hex_plot <- function(out, mult = 1){
-  sticker(img, 1, 0.8, 0.8, 1, pkg, p_size = mult * 48, h_size = mult * 1.2, h_fill = "firebrick1",
-          h_color = "gray20", url = paste0("leonawicz.github.io/", pkg), u_color = "white", u_size = mult * 3,
-          filename = out)
+  sticker("data-raw/images/hexsubplot2.png", 1, 1, 0.8, 0.8, "TREK", p_size = mult * 36, p_y = 1, p_color = "#63B8FFDD",
+          p_family = "tng", h_size = mult * 1, h_fill = "#001030", h_color = "slategray1",
+          url = paste0("leonawicz.github.io/", pkg), u_color = "slategray1", u_size = mult * 3, filename = out)
   # overwrite file for larger size
-  if(mult != 1) ggsave(out, width = mult*43.9, height = mult*50.8, bg = "transparent", units = "mm")
+  if(mult != 1) ggplot2::ggsave(out, width = mult*43.9, height = mult*50.8, bg = "transparent", units = "mm")
 }
 
+# Both logo versions in data-raw; neither published inside package
 hex_plot(out[1], 4) # multiplier for larger sticker size
 hex_plot(out[2])
