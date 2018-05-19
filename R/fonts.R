7d0bd6c74f27adb762a6e253a9a53295d2d4934e
leonawicz <matt.rstat@gmail.com>
2018-05-18 22:33:03 -0600

    Improve st_font, clean up code and examples. Update arguments.

--- R/fonts.R
+++ R/fonts.R
@@ -7,16 +7,17 @@
 #' In RStudio on Windows the font may not show in the RStudio graphics device. Try using the regular R GUI.
 #'
 #' @param family character, font family.
+#' @param size, numeric, font size passed to ggplot.
 #'
 #' @return a character vector, or a plot side effect. See details.
 #' @export
 #'
 #' @examples
-#' library(showtext)
-#' showtect_auto()
-#' st_font()
-#' st_font("Federation")
-st_font <- function(family){
+#' if(all(c("trekfont", "showtext", "ggplot2") %in% installed.packages())){
+#'   st_font()
+#' }
+#' \dontrun{st_font("Federation")} # should be run in an interactive session
+st_font <- function(family, size = 11){
   if(!requireNamespace("trekfont", quietly = TRUE)){
     message("The `trekfont` package is not installed.")
     return(invisible())
@@ -40,18 +41,18 @@ st_font <- function(family){
   x <- c(tolower(x), x, toupper(x))
   x2 <- '"Captain, this font is strong.\nI recommend extreme caution."'
   showtext::showtext_auto()
-  ggplot2::ggplot() + ggplot2::theme_gray(base_family = "stfont") +
+  ggplot2::ggplot() + ggplot2::theme_gray(base_size = size, base_family = "stfont") +
     ggplot2::theme(plot.margin = grid::unit(c(2, 4, 2, 2), "mm")) +
     ggplot2::coord_cartesian(xlim = c(0, 1), ylim = c(0, 1)) +
     ggplot2::scale_x_continuous(expand = c(0.01, 0)) +
     ggplot2::scale_y_continuous(expand = c(0.01, 0)) +
     ggplot2::annotation_custom(g, xmin = 0.7, xmax = 1, ymin = 0.72, ymax = Inf) +
     ggplot2::labs(title = x0a, subtitle = x0b) +
-    ggplot2::annotate("text", x = 0, y = 0.5, hjust = 0, label = x[1], family = "stfont") +
-    ggplot2::annotate("text", x = 0, y = 0.43, hjust = 0, label = x[2], family = "stfont") +
-    ggplot2::annotate("text", x = 0, y = 0.36, hjust = 0, label = x[3], family = "stfont") +
+    ggplot2::annotate("text", x = 0, y = 0.5, hjust = 0, size = size/3, label = x[1], family = "stfont") +
+    ggplot2::annotate("text", x = 0, y = 0.43, hjust = 0, size = size/3, label = x[2], family = "stfont") +
+    ggplot2::annotate("text", x = 0, y = 0.36, hjust = 0, size = size/3, label = x[3], family = "stfont") +
     ggplot2::annotate("text", x = 0, y = 0.17, hjust = 0,
                       label = paste0(tolower(x0a), "\nJUMPS OVER\nTHE LAZY DOG."),
-                      family = "stfont", size = 11) +
-    ggplot2::annotate("text", x = 1, y = 0.65, hjust = 1, label = x2, family = "stfont")
+                      family = "stfont", size = size) +
+    ggplot2::annotate("text", x = 1, y = 0.65, hjust = 1, size = size/3, label = x2, family = "stfont")
 }
