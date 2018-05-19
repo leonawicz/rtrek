2333481e51dd118e47d8580cc83d4095b0b942d7
leonawicz <matt.rstat@gmail.com>
2018-05-19 13:19:09 -0600

    Skip test on appveyor.

--- tests/testthat/test-fonts.R
+++ tests/testthat/test-fonts.R
@@ -1,6 +1,8 @@
 context("fonts")
 
 test_that("st_font returns as expected when optional packages are installed", {
+  skip_on_appveyor()
+
   x <- st_font()
   expect_is(x, "character")
   expect_equal(length(x), 107)
