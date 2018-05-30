f7c9ee6c73495323d80db1bd89dba9e10570cdd6
leonawicz <matt.rstat@gmail.com>
2018-05-29 19:57:52 -0600

    Refactor raw data script.

--- data-raw/data.R
+++ data-raw/data.R
@@ -1,39 +1,53 @@
 library(dplyr)
 
 # minor package datasets
-.species <- c("Human", "Romulan", "Klingon", "Breen", "Ferengi", "Cardassian", "Tholian")
+.st_species <- c("Human", "Romulan", "Klingon", "Breen", "Ferengi", "Cardassian", "Tholian", "Tzenkethi", "Talarian")
+.st_locs <- c("Earth", "Romulus", "Qo'noS", "Breen", "Ferenginar", "Cardassia", "Tholia", "Tzenketh", "Talar")
+.st_zone <- c("United Federation of Planets", "Romulan Star Empire", "Klingon Empire", "Breen Confederacy",
+              "Ferengi Alliance", "Cardassian Union", "Tholian Assembly", "Tzenkethi Coalition", "Talarian Republic")
+.tile_ids <- c("galaxy1", "galaxy2")
 
-stGeo <- data_frame(
-  col = c(2196, 2615, 3310, 1004, 1431, 1342, 407),
-  row = c(2357, 1742, 3361, 939, 1996, 2841, 3866),
-  label = c("Earth", "Romulus", "Qo'noS", "Breen", "Ferenginar", "Cardassia", "Tholia"),
-  body = "Planet", category = "Homeworld",
-  zone = c("United Federation of Planets", "Romulan Star Empire", "Klingon Empire", "Breen Confederacy",
-           "Ferengi Alliance", "Cardassian Union", "Tholian Assembly"),
-  species = .species)
+.gc01 <- data_frame(
+  id = .tile_ids[1],
+  loc = .st_locs,
+  col = c(2196, 2615, 3310, 1004, 1431, 1342, 407, 1553, 1039),
+  row = c(2357, 1742, 3361, 939, 1996, 2841, 3866, 2557, 3489)
+)
+.gc02 <- data_frame(
+  id = .tile_ids[2],
+  loc = .st_locs,
+  col = c(2201, 2514, 3197, 1228, 2026, 1543, 713,  1734, 1338),
+  row = c(1595, 1178, 2303, 1181, 886,  1903, 2971, 1721, 2368)
+)
+stGeo <- bind_rows(.gc01, .gc02)
 
 stSpecies <- data_frame(
-  species = .species,
-  avatar = paste0("https://vignette.wikia.nocookie.net/memoryalpha/images/", c(
+  species = .st_species,
+  avatar = c(paste0("https://vignette.wikia.nocookie.net/memoryalpha/images/", c(
     "7/77/Hoshi_Sato%2C_2154.jpg/revision/latest/scale-to-width-down/350?cb=20120409224834",
     "2/25/Toreth.jpg/revision/latest/scale-to-width-down/292?cb=20140713231352",
     "c/c6/Martok%2C_Chancellor_of_the_Klingon_High_Council.jpg/revision/latest/scale-to-width-down/292?cb=20110805121635",
     "0/07/Pran.jpg/revision/latest/scale-to-width-down/292?cb=20060128135314",
     "2/28/Quark%2C_2375.jpg/revision/latest/scale-to-width-down/350?cb=20120329231622",
     "7/79/Dukat%2C_2375.jpg/revision/latest/scale-to-width-down/350?cb=20120418042102",
-    "4/48/Tholian%2C_2155.jpg/revision/latest/scale-to-width-down/292?cb=20110905051618"
-  ), "&path-prefix=en")
+    "4/48/Tholian%2C_2155.jpg/revision/latest/scale-to-width-down/292?cb=20110905051618",
+    "b/bc/Endar.jpg/revision/latest?cb=20140207112627"
+  ), "&path-prefix=en"),
+  "https://pwimages-a.akamaihd.net/arc/b1/c9/b1c942a7297c22abf5e375a9f60217681505520656.jpg")
 )
 
 stTiles <- data_frame(
-  id = "galaxy1",
-  url = "https://leonawicz.github.io/tiles/data/st1/{z}/{x}/{y}.png",
-  description = "Geopolitical galaxy map",
-  width = 8000,
-  height = 6445,
-  tile_creator = "Matthew Leonawicz",
-  map_creator = "Rob Archer",
-  map_url = "https://archerxx.deviantart.com/art/Star-Trek-Star-Chart-316982311"
+  id = .tile_ids,
+  url = paste0("https://leonawicz.github.io/tiles/data/", c("st1", "st2"), "/{z}/{x}/{y}.png"),
+  description = rep("Geopolitical galaxy map", 2),
+  width = c(8000, 5000),
+  height = c(6445, 4000),
+  tile_creator = rep("Matthew Leonawicz", 2),
+  map_creator = c("Rob Archer", NA),
+  map_url = c(
+    "https://archerxx.deviantart.com/art/Star-Trek-Star-Chart-316982311",
+    "http://sttff.net/AST_MAP.html"
+  )
 )
 
 # create table of STAPI entity info
@@ -70,3 +84,11 @@ attr(stapiEntities, "ignored columns") <- dropcols
 
 # save datasets
 usethis::use_data(stGeo, stSpecies, stTiles, stapiEntities)
+
+# internal data
+library(jpeg)
+.worf <- readJPEG("data-raw/images/worf.jpg")
+
+stBooksId <- readRDS("data-raw/internal/stBooksId.rds")
+
+usethis::use_data(stBooksId, .worf, .st_species, .st_zone, internal = TRUE)
