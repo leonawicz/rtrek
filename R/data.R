0c4ad024a58211b99aa87a6ac7b2705a07d36dc7
Matt Leonawicz <matt.rstat@gmail.com>
2018-10-28 14:49:13 -0600

    Update R scripts related to updating existing datasets and adding new datasets. Update related package documentation. Add stSeries, tlBooks, tlEvents, tlFootnotes. Rename Wikipedia novels dataset and function.

--- R/data.R
+++ R/data.R
@@ -21,31 +21,47 @@
 
 #' Star Trek novel metadata from Wikipedia.
 #'
-#' A data frame with 715 rows and 6 columns containing metadata on Star Trek novels and other books taken from the primary Wikipedia page: https://en.wikipedia.org/wiki/List_of_Star_Trek_novels.
+#' A data frame with 719 rows and 6 columns containing metadata on Star Trek novels and other books taken from the primary Wikipedia page: \url{https://en.wikipedia.org/wiki/List_of_Star_Trek_novels}.
 #' The data frame contains most of the novels but is not comprehensive, containing only the most easily scraped HTML table data, and may be out of date temporarily whenever new novels are published.
 #'
-#' There is considerable overlap in titles between \code{stBooksWP} and \code{stBooks}, but also a considerable number of unique entries.
-#' For example, the old novelizations by James Blish of the Original Series episodes are not found in \code{stBooks}.
+#' There is considerable overlap in titles between \code{stBooksWiki} and \code{stBooks}, but also unique entries and they offer some different fields.
 #'
 #' @format A data frame
-#' @seealso \code{\link{st_book_series}}, \code{\link{stBooks}}
-"stBooksWP"
+#' @seealso \code{\link{st_books_wiki}}, \code{\link{stBooks}}
+"stBooksWiki"
 
 #' Star Trek novel metadata.
 #'
-#' A data frame with 743 rows and 11 columns containing metadata on Star Trek novels and other books taken from original books.
+#' A data frame with 783 rows and 11 columns containing metadata on Star Trek novels and other books taken directly from original books.
 #' The data frame contains most of the novels but is not comprehensive and may be out of date temporarily whenever new novels are published.
+#' It is largely complete through the end of 2017, though some older entries are still missing.
 #'
-#' There is considerable overlap in titles between \code{stBooksWP} and \code{stBooks}, but also a considerable number of unique entries.
-#' For example, the old novelizations by James Blish of the Original Series episodes are not found in \code{stBooks}. This dataset only goes back as far as 1979.
-#'
-#' However, \code{stBooks} contains a number of additional columns providing metadata about each book that could only be parsed from books and not from the Wikipedia page that serves as the source for \code{stBooksWP}.
+#' \code{stBooks} contains a number of additional columns providing metadata about each book that could only be parsed directly from books and not from the Wikipedia page that serves as the source for \code{stBooksWiki}.
 #' These columns include the number of characters, words and chapters in a book. There may be some irregularities or erroneous entries based on the imperfect methods use to compile the metadata,
 #' but it is overall an accurate dataset.
 #'
+#' The \code{nchap} column is largely accurate, but imperfect. Some entries suggest a book has hundreds of chapters, but the parser is not perfect at determining what constitutes a chapter.
+#' However, many of the books with high numbers of chapters are not erroneous but rather indicate a reference book rather than a novel, or an omnibus or anthology.
+#'
 #' @format A data frame
-#' @seealso \code{\link{st_book_series}}, \code{\link{stBooksWP}}
+#' @seealso \code{\link{stSeries}}, \code{\link{stBooksWiki}}
 "stBooks"
+
+#' Star Trek series.
+#'
+#' A data frame with 35 rows and 3 columns containing names and abbreviations of Star Trek series and anthologies.
+#' There are so many because the table pertains to written works, which is inclusive of the more limited televised series.
+#'
+#' Some entries listed as series can be interpreted as miniseries, but that distinction is not made here.
+#' The official line between the two is not always clear and can also change as more novels are released.
+#'
+#' Anthologies are listed as such, rather than as series. Reference manuals have a distinct entry.
+#' The Miscellaneous category can be considered synonymous with All-Series/Crossover,
+#' abbreviated elsewhere as simply \code{ST} for Star Trek in general, rather than as \code{MISC}.
+#'
+#' @format A data frame
+#' @seealso \code{\link{st_books_wiki}}
+"stSeries"
 
 #' Star Trek API entities.
 #'
@@ -58,6 +74,52 @@
 #' @seealso \code{\link{stapi}}
 "stapiEntities"
 
+#' Star Trek novel-based timeline.
+#'
+#' A data frame with 2122 rows and 14 columns containing Star Trek timeline data. This dataset is novel-driven,
+#' meaning that the timeline entries (rows) provide a chronologically ordered list of licensed Star Trek novels.
+#'
+#' Specifically, this curated dataset includes data derived from historical timeline information in the appendix of the Star Trek reference manual, Voyages of the Imagination,
+#' which provides information on the large collection of licensed Star Trek literature.
+#' The authors note that the original timeline includes "novels, short stories, eBooks, novelizations, Simon & Schuster Audio original audio books, Minstrel Books young adult books, and classic novels from Bantam and Ballentine Books, published through October 2006."
+#'
+#' While this data is very informative, it is clearly many years out of date. It is also necessarily speculative.
+#' Settings are determined based in part on what is interpreted to be the intention of a given author for a given production.
+#' Nevertheless, it still represents possibly the highest quality representation of the chronological ordering of Star Trek fiction that combines episodes and movies with written works.
+#' The concurrent timeline of Star Trek TV episodes and movies are interleaved with the novels and other written fiction for fuller context resulting in a much richer timeline.
+#' See the \code{tlEvents} dataset for an event-driven timeline.
+#'
+#' @format A data frame
+#' @seealso \code{\link{tlEvents}} \code{\link{tlFootnotes}}
+"tlBooks"
+
+#' Star Trek event-based timeline.
+#'
+#' A data frame with 1241 rows and 6 columns containing Star Trek timeline data. This dataset is event-driven,
+#' meaning that the timeline entries (rows) provide chronologically ordered historical events from the Star Trek universe.
+#' See the \code{tlEvents} dataset for an event-driven timeline.
+#'
+#' This timeline was recreated from the timeline found at \url{http://www.maplenet.net/~trowbridge/timeline.htm}.
+#' As with \code{tlBooks}, this timeline is quite out of date. In fact it is at least somewhat more out of date than \code{tlBooks}.
+#' This timeline is also more problematic than the other, and less relevant moving forward. Its updating essentially ceased  as the other began.
+#' See the URL for more details.
+#'
+#' However, it is included because unlike \code{tlBooks}, which is a timeline of production titles, this timeline dataset is event-driven.
+#' While it may now be erroneous in places even independent from being out of date,
+#' it is useful for its informative textual entries referencing historically significant events in Star Trek lore.
+#'
+#' @format A data frame
+#' @seealso \code{\link{tlBooks}} \code{\link{tlFootnotes}}
+"tlEvents"
+
+#' Star Trek timeline footnotes.
+#'
+#' A data frame with 605 rows and 3 columns containing footnotes associated by ID with various entries in package timeline datasets, \code{tlBooks} and \code{tlEvents}.
+#'
+#' @format A data frame
+#' @seealso \code{\link{tlBooks}} \code{\link{tlEvents}}
+"tlFootnotes"
+
 #' Available datasets
 #'
 #' List the available datasets in the rtrek package.
@@ -69,8 +131,18 @@
 #' st_datasets()
 st_datasets <- function(){
   dplyr::data_frame(
-    dataset = c("stGeo", "stSpecies", "stTiles", "stBooks", "stBooksWP", "stapiEntities"),
-    description = c("Map tile set locations of interest.", "Basic intelligent species data.",
-                    "Available map tile sets.", "Star Trek novel metadata.", "Star Trek novel metadata from Wikipedia.",
-                    "Star Trek API (STAPI) categories"))
+    dataset = c("stGeo", "stSpecies", "stTiles",
+                "stBooks", "stBooksWiki", "stSeries",
+                "stapiEntities",
+                "tlBooks", "tlEvents", "tlFootnotes"),
+    description = c("Map tile set locations of interest.",
+                    "Basic intelligent species data.",
+                    "Available map tile sets.",
+                    "Star Trek novel metadata.",
+                    "Star Trek novel metadata from Wikipedia.",
+                    "Names and acronyms of Star Trek series",
+                    "Star Trek API (STAPI) categories",
+                    "Novel-based timeline dataset",
+                    "Event-based timeline dataset",
+                    "Timeline dataset footnotes"))
 }
--- R/novels.R
+++ R/novels.R
@@ -1,18 +1,20 @@
 #' Go to Wikipedia entry for a specific book series
 #'
 #' This function opens a browser tab to the main Wikipedia entry for all Star Trek novels automatically scrolled to the section pertaining to \code{id}.
-#' To see the available IDs, call \code{st_book_series} with no arguments. The Wikipedia page is the one that serves as the source for the \code{stBooksWP} dataset.
+#' To see the available IDs, call \code{st_books_wiki} with no arguments. The Wikipedia page is the one that serves as the source for the \code{stBooksWiki} dataset.
+#'
+#' The search IDs available in \code{st_books_wiki} do not represent an exhaustive set of Star Trek series.
+#' For a more complete set of Star Trek series, miniseries and anthology names and acronyms, see the \code{\link{stSeries}} dataset.
 #'
 #' @param id character, abbreviation for a series. See details.
 #'
 #' @return opens a browser tab, nothing is returned unless \code{id} is not provided, in which case a data frame is returned.
 #' @export
-#' @seealso \code{\link{stBooksWP}}
+#' @seealso \code{\link{stBooksWiki}} \code{\link{stSeries}}
 #'
 #' @examples
-#' st_book_series()
-#' st_book_series("DS9")
-st_book_series <- function(id){
+#' st_books_wiki("DS9")
+st_books_wiki <- function(id){
   x <- stBooksId
   if(missing(id)) return(dplyr::select(x, -.data[["id"]]))
   id <- x$id[x$abb == id]
