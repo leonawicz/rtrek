ab65a229378b95f537771a743fa4f756b9e0428d
leonawicz <matt_leonawicz@esource.com>
2018-11-21 19:15:30 -0700

    Add data prep script.

---
+++ data-raw/timeline.R
@@ -0,0 +1,22 @@
+library(trekdata)
+library(dplyr)
+
+file <- "data-raw/support_files/Reference/20061114 REF - Voyages of Imagination.epub"
+x <- epubr::epub(file)
+txt <- slice(x$data[[1]], match(c("bm_2", "bm_2a", "bm_2b"), x$data[[1]]$section))$text
+txt <- paste0(txt, collapse = "\n")
+txt <- strsplit(txt, "\n")[[1]] %>% tl_clean_text()
+
+tlBookNotes <- tl_footnotes(txt)
+tlBooks <- tl_entries(txt) %>% bind_rows()
+
+tlEvents <- readr::read_csv("data-raw/st-timeline-2.csv")
+idx <- which(!is.na(tlEvents$note))
+tlEventNotes <- data_frame(id = seq_along(idx), text = tlEvents$note[idx])
+tlEvents$note <- as.integer(NA)
+tlEvents$note[idx] <- seq_along(idx)
+tlEvents <- rename(tlEvents, year = date, footnote = note) %>% select(c(1:3, 6:4))
+
+tlFootnotes <- bind_rows(book = tlBookNotes, event = tlEventNotes, .id = "id")
+
+usethis::use_data(tlBooks, tlEvents, tlFootnotes)
