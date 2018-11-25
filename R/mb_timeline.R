9d9b37d3b21ea924b224eb0702dcc61f556623ae
leonawicz <matt_leonawicz@esource.com>
2018-11-25 15:15:31 -0700

    Continue Memory Beta timeline functions development. Address several edge cases, bugs, anti-ddos measures, accessing different sections of timeline in different ways but with a single interface.

--- R/mb_timeline.R
+++ R/mb_timeline.R
@@ -1,4 +1,30 @@
-mb_timeline <- function(){
+mb_timeline <- function(x){
+  if(is.numeric(x)){
+    purrr::map(as.character(x), .mb_tl_list) %>% purrr::transpose() %>% purrr::map(dplyr::bind_rows)
+  } else {
+    if(length(x) > 1){
+      stop("`x` can only be a vector for numeric years. Charatcer options must be scalar.", call. = FALSE)
+    }
+    if(grepl("^\\d\\d\\d\\ds$", x)){
+      if(x < "1900s" | x > "2490s")
+        stop(paste("`x` must be in the range '1900s' - '2490s'.",
+                   "For distant past/future use x = 'past'/x = 'future'."), call. = FALSE)
+      mb_tl_by_decade_year(min = x, max = x)
+    } else {
+      if(!x %in% c("main", "past", "future", "complete"))
+        stop(paste("`x` must be numeric years, e.g., 2371:2374, or character decade, e.g. '2370s',",
+                    "or one of 'main', 'past', 'future' or 'complete'."), call. = FALSE)
+      switch(x, main = mb_tl_by_decade_year(), past = mb_tl_past(), future = mb_tl_future(), complete = mb_tl_complete())
+    }
+  }
+}
+
+mb_tl_complete <- function(){
+  list(mb_tl_past(), mb_tl_by_decade_year(), mb_tl_future()) %>%
+    purrr::transpose() %>% purrr::map(dplyr::bind_rows)
+}
+
+mb_tl_past <- function(){
   distant_past <- c("Billions_of_years_ago", "Millions_of_years_ago", "Thousands_of_years_ago")
   x0 <- purrr::map(distant_past, .mb_tl_list) %>% purrr::transpose() %>% purrr::map(dplyr::bind_rows)
 
@@ -17,9 +43,68 @@ mb_timeline <- function(){
   x0
 }
 
-.mb_tl_list <- function(url, stories = TRUE){
-  x <- xml2::read_html(mb_base_add(url)) %>%
-    rvest::html_nodes(".WikiaMainContent > .WikiaMainContentContainer > .WikiaArticle > #mw-content-text") %>%
+mb_tl_past <- memoise::memoise(mb_tl_past)
+
+mb_tl_future <- function(){
+  x <- mb_base_add("Memory_Beta_Chronology:_Far_Future") %>% xml2::read_html() %>%
+    rvest::html_nodes(".WikiaMainContent > .WikiaMainContentContainer > .WikiaArticle > #mw-content-text")
+  x2 <- x %>% rvest::html_nodes(".mw-headline > a") %>% mb_href()
+  events <- purrr::map(x2, .mb_tl_list, stories = FALSE) %>% purrr::transpose() %>% `[[`(1) %>%
+    dplyr::bind_rows()
+
+  tbls <- x %>% rvest::html_nodes("table")
+  tbls <- tbls[is.na(rvest::html_attr(tbls, "class"))]
+  stories <- purrr::map_dfr(seq_along(tbls), ~.mb_stories_df(tbls[.x]))
+  list(events = events, stories = stories)
+}
+
+mb_tl_future <- memoise::memoise(mb_tl_future)
+
+mb_tl_by_decade_year <- function(min = "1900s", max = "2490s"){
+  decades <- memory_beta("timeline/Decades") %>%
+    dplyr::filter(.data[["url"]] != "Decade" & !grepl("^Category:", .data[["url"]]) &
+                    .data[["url"]] >= min & .data[["url"]] <= max)
+  pages <- purrr::map(decades$url, decade_year_pages) %>% purrr::flatten()
+  purrr::map(seq_along(pages), ~.mb_tl_list(pages[.x])) %>%
+    purrr::transpose() %>% purrr::map(dplyr::bind_rows)
+}
+
+mb_tl_safe_read <- function(url){
+  .antiddos("mbtl")
+  cat(url, "\n")
+  x <- xml2::read_html(mb_base_add(url))
+  assign("mbtl", Sys.time(), envir = rtrek_api_time)
+  x
+}
+
+decade_year_pages <- function(url){
+  x0 <- mb_tl_safe_read(url)
+  x <- rvest::html_nodes(x0, ".WikiaMainContent > .WikiaMainContentContainer > .WikiaArticle > #mw-content-text")
+  if(!length(rvest::html_nodes(x, "#Years"))){
+    x0 <- list(x0)
+    names(x0) <- url
+    return(x0)
+  }
+  all_years <- as.character(as.integer(substr(url, 1, 4)) + 0:9)
+  x <- rvest::html_nodes(x, "ul > li > a") %>% mb_href()
+  url <- intersect(x, all_years)
+  x <- purrr::map(url, mb_tl_safe_read)
+  names(x) <- url
+  x
+}
+
+decade_year_pages <- memoise::memoise(decade_year_pages)
+
+.mb_tl_list <- function(x, stories = TRUE){
+  if(inherits(x, "character")){
+    url <- x
+    x <- mb_tl_safe_read(url)
+  } else {
+    url <- names(x)
+    x <- x[[1]]
+  }
+
+  x <- rvest::html_nodes(x, ".WikiaMainContent > .WikiaMainContentContainer > .WikiaArticle > #mw-content-text") %>%
     rvest::html_nodes(".mw-headline, dt, dd, li, table")
   ids <- rvest::html_attr(x, "id")
   idx <- grep("^External_link", ids)
@@ -28,11 +113,14 @@ mb_timeline <- function(){
     ids <- ids[1:(idx - 1)]
   }
 
-  x <- x[is.na(ids) | !grepl("^cite|^Connections|^Appendices|^References|^External|^toc$", ids)]
+  d <- tibble::data_frame(period = gsub("_", " ", url), id = ids) %>%
+    tidyr::fill(!!"id")
+  idx <- !grepl("^cite|^Connections|^Appendices|^References|^External|^toc$", d$id)
+  d <- dplyr::filter(d, idx)
+  x <- x[idx]
   ids <- rvest::html_attr(x, "id")
   idx <- which(is.na(ids))
-  d <- tibble::data_frame(period = gsub("_", " ", url), id = ids) %>%
-    tidyr::fill(!!"id") %>% dplyr::slice(idx)
+  d <- dplyr::slice(d, idx)
   x <- x[idx]
   ids <- rvest::html_name(x)
 
@@ -41,24 +129,38 @@ mb_timeline <- function(){
   d <- dplyr::mutate(d, date = txt) %>% tidyr::fill(!!"date") %>% dplyr::slice(idx)
   d$date[d$id == "Stories"] <- NA
   x <- x[idx]
+  ids <- rvest::html_name(x)
+
+  idx <- which((ids == "table" & d$id != "Stories"))
+  if(length(idx)){
+    d <- dplyr::slice(d, -idx)
+    x <- x[-idx]
+  }
 
   d <- dplyr::mutate(d, node = purrr::map(seq_along(x), ~x[.x]))
-  if(stories) ds <- dplyr::filter(d, .data[["id"]] == "Stories")
+  if(!"Stories" %in% d$id) stories <- FALSE
+  if(stories){
+    ds <- dplyr::filter(d, .data[["id"]] == "Stories")
+    if(!rvest::html_name(ds$node[[1]]) == "table") stories <- FALSE
+  }
   d <- dplyr::filter(d, .data[["id"]] != "Stories")
 
   d$notes <- sapply(d$node, mb_text)
   d <- dplyr::select(d, -.data[["node"]]) %>% dplyr::filter(.data[["notes"]] != "")
   d <- .mb_fix_date_in_notes(d)
+  d <- dplyr::mutate_if(d, function(x) !is.character(x), as.character)
   if(nrow(d) == 0) d <- NULL
   ds <- if(stories) .mb_stories_df(ds$node[[1]]) else NULL
   list(events = d, stories = ds)
 }
 
+.mb_tl_list <- memoise::memoise(.mb_tl_list)
+
 .mb_fix_date_in_notes <- function(x){
-  idx <- grep("^(\\d+: |^Circa \\d+: )", x$notes)
+  idx <- grep("^(\\d+: |^Circa \\d+: |\\d\\d\\d\\d )", x$notes)
   if(length(idx)){
-    x$date[idx] <- gsub("^(\\d+|^Circa \\d+): .*", "\\1", x$notes[idx])
-    x$notes[idx] <- gsub("^(\\d+: |^Circa \\d+: )(.*)", "\\2", x$notes[idx])
+    x$date[idx] <- gsub("^(\\d+|^Circa \\d+|\\d\\d\\d\\d)(: | ).*", "\\1", x$notes[idx])
+    x$notes[idx] <- gsub("^(\\d+: |^Circa \\d+: |\\d\\d\\d\\d )(.*)", "\\2", x$notes[idx])
   }
   x
 }
@@ -78,18 +180,18 @@ mb_timeline <- function(){
 .mb_stories_titles <- function(x){
   title_nodes <- rvest::html_nodes(x, "tr") %>% `[`(-1) %>% purrr::map(~rvest::html_nodes(.x, "td")[1])
   titles <- purrr::map(title_nodes, ~rvest::html_nodes(.x, "b > a"))
-  title_lab <- purrr::map_chr(titles, ~{if(length(.x)) mb_text(.x) else as.character(NA)})
-  title_url <- purrr::map_chr(titles, ~{if(length(.x)) mb_href(.x) else as.character(NA)})
+  title_lab <- purrr::map_chr(titles, ~{if(length(.x)) paste0(mb_text(.x), collapse = "; ") else as.character(NA)})
+  title_url <- purrr::map_chr(titles, ~{if(length(.x)) paste0(mb_href(.x), collapse = "; ") else as.character(NA)})
 
   ms <- purrr::map(title_nodes, ~rvest::html_nodes(.x, "small > a"))
-  ms_lab <- purrr::map_chr(ms, ~{if(length(.x)) mb_text(.x) else as.character(NA)})
-  ms_url <- purrr::map_chr(ms, ~{if(length(.x)) mb_href(.x) else as.character(NA)})
+  ms_lab <- purrr::map_chr(ms, ~{if(length(.x)) paste0(mb_text(.x), collapse = "; ") else as.character(NA)})
+  ms_url <- purrr::map_chr(ms, ~{if(length(.x)) paste0(mb_href(.x), collapse = "; ") else as.character(NA)})
 
   section <- purrr::map_chr(title_nodes, ~{
     x <- rvest::html_nodes(.x, "small") %>% xml2::xml_contents()
     idx <- which(rvest::html_name(x) == "text")
-    if(length(idx)) x <- mb_text(x[idx]) else return(as.character(NA))
-    x <- x[!x %in% c("(", ")")]
+    if(length(idx)) x <- paste0(mb_text(x[idx]), collapse = "; ") else return(as.character(NA))
+    x <- x[!x %in% c("(", ")", ":")]
     if(!length(x)) x <- as.character(NA)
     x
   })
